a10c602160430b8948fd35ca3390141356e88808
Данила Аносов <pike123@router.lab209.kaf>
2021-10-07 06:35:08 +0300

    fork wait() modified

--- lab1/main.c
+++ lab1/main.c
@@ -14,6 +14,7 @@ void exitfunc()
 int main() { 
 	pid_t pid = fork(); 
 	atexit(exitfunc);
+	int status, exint = 0;
 	switch(pid) { 
 		case -1:
 			perror("fork");  /* произошла ошибка */
@@ -22,15 +23,17 @@ int main() {
 			printf(" CHILD: Это процесс-потомок!\n");
 			printf(" CHILD: Мой PID -- %d\n", getpid()); 
 			printf(" CHILD: PID моего родителя -- %d\n", getppid()); 
-			printf(" CHILD: Выход!\n"); 
-			exit(0); 
+			printf(" CHILD: Выход!\n");
+			printf(" CHILD: Введите код выхода: \n");
+			scanf("%d", &exint);
+			exit(exint); 
 		default: 
 			printf("PARENT: Это процесс-родитель!\n");
 			printf("PARENT: Мой PID -- %d\n", getpid()); 
 			printf("PARENT: PID моего потомка %d\n",pid); 	
 			printf("PARENT: Я жду, пока потомок не вызовет exit()...\n");
-		       	wait(NULL); 
-			printf("PARENT: Код возврата потомка:%d\n", WEXITSTATUS(0)); 
+		       	wait(&status); 
+			printf("PARENT: Код возврата потомка:%d\n", WEXITSTATUS(status)); 
 			printf("PARENT: Выход!\n"); 
 				} 
 	return 0;
